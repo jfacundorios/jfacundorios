dd39319cea5c81a9406844f3e64e823a9ce2e194
Facundo Rios <112650077+jfacundorios@users.noreply.github.com>
2023-08-01 14:42:31 -0300

    Add files via upload

---
+++ C++/tiposdedatsobasicos.cpp
@@ -0,0 +1,20 @@
+//Tipos de datos basicos de C++
+
+#include<iostream>
+
+int main(){
+    int entero = 15;
+    float flotante = 10.45;
+    double mayor = 16.3456
+    char letra = '1';
+
+
+    cout<<flotante;
+    cout<<entero;
+    cout<<mayor;
+    cout<<letra;
+ 
+
+
+    return 0;
+}
